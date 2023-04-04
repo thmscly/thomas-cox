8b8706911c829ea126bd56188ce57907e3460fa5
T.C. Cox <thmscly@gmail.com>
2023-04-04 14:05:44 -0400

    Created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import React from "react";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import "./App.css";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 function App() {
-  <Home />;
+  (<Home />), (<About />), (<Projects />), (<Contact />);
 }
 
 export default App;
