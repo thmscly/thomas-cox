1affcc494a896abf9a0c804e1d6923776e2a046f
T.C. Cox <thmscly@gmail.com>
2023-04-05 13:19:03 -0400

    added div(w-4/5) and grid wrapper to App

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import Footer from "./layouts/Footer";
 
 function App() {
   return (
-    <div className="text-green-600 font-mono min-w-screen min-h-screen bg-black pb-2">
-      <div>
-        <NavBar />
+    <div className="grid justify-items-center text-green-600 font-mono min-w-screen min-h-screen bg-black pb-2">
+      <div className="w-4/5">
+        <div>
+          <NavBar />
+        </div>
+        <div className="grid grid-flow-row gap-5 mt-10">
+          <Routing />
+        </div>
+        <Footer />
       </div>
-      <div className="grid grid-flow-row gap-5 mt-10">
-        <Routing />
-      </div>
-      <Footer />
     </div>
   );
 }
