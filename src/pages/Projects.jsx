905bd2a81fd2ccc6b55798684dc2b30f48844164
T.C. Cox <thmscly@gmail.com>
2023-04-05 13:24:12 -0400

    added info prop to Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,16 +6,19 @@ const info = "A look into some of the projects I've enjoyed working on.";
 const content = (
   <div className="grid grid-cols-3 gap-5">
     <div>
-      <h3>Meta Developer Course Capstone</h3>
-      <p>blahblahablhablhababalhablahablblahblblahablah</p>
+      <h3>Meta Certification Capstone</h3>
+      <p>
+        The Capstone project for Coursera's Meta Front-End Developer
+        Professional Certificate
+      </p>
     </div>
     <div>
       <h3>Meta Developer Course Capstone</h3>
-      <p>blahblahablhablhababalhablahablblahblblahablah</p>
+      <p>blahblah ablhablha babalhablah ablb lahblblaha blah</p>
     </div>
     <div>
       <h3>Meta Developer Course Capstone</h3>
-      <p>blahblahablhablhababalhablahablblahblblahablah</p>
+      <p>blahblah ablhablha babalhablah ablb lahblblaha blah</p>
     </div>
   </div>
 );
@@ -23,7 +26,7 @@ const content = (
 function Projects() {
   return (
     <div>
-      <PageLayout title={title} content={content} />
+      <PageLayout title={title} content={content} info={info} />
     </div>
   );
 }
